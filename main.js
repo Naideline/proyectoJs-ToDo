51171dce2d797469ac14162f5d94531d32035f2c
Naideline <107892053+Naideline@users.noreply.github.com>
2023-03-24 21:45:55 -0600

    Time and remove button

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-//selectores
 const todoInput = document.querySelector(".todo_input");
 const todoButton = document.querySelector(".todo_button");
 const todoList = document.querySelector(".todo_list");
-const filterOption = document.querySelector(".filter_todo");
+const nombreTarea = document.getElementById("nombre_tarea");
+const categoriaTarea = document.getElementById("categoria_tarea");
+
+// Habilitar/deshabilitar el botón de "Añadir tarea" según los campos tengan un valor o no
+nombreTarea.addEventListener("input", validarCampos);
+categoriaTarea.addEventListener("input", validarCampos);
+
+function validarCampos() {
+  if (nombreTarea.value !== "" && categoriaTarea.value !== "") {
+    todoButton.disabled = false;
+  } else {
+    todoButton.disabled = true;
+  }
+}
 
 //event listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
-filterOption.addEventListener("click", filterTodo);
 
 //funciones
 function addTodo(event) {
@@ -20,6 +31,16 @@ function addTodo(event) {
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo_item");
   todoDiv.appendChild(newTodo);
+
+  // Tiempo transcurrido
+  const now = new Date();
+  const timeSpan = document.createElement("span");
+  timeSpan.classList.add("time_span");
+  timeSpan.innerText = getElapsedTime(now);
+  newTodo.appendChild(timeSpan);
+
+  newTodo.setAttribute("data-created", now);
+
   if (todoInput.value === "") {
     return null;
   }
@@ -37,9 +58,35 @@ function addTodo(event) {
   todoList.appendChild(todoDiv);
   //LIMPIAR EL INPUT
   todoInput.value = "";
-	
 }
 
+function getElapsedTime(created) {
+  const now = new Date();
+  const diff = now - created;
+
+  const seconds = Math.floor(diff / 1000);
+  if (seconds < 60) {
+    return `     / ${seconds} seconds ago`;
+  }
+
+  const minutes = Math.floor(diff / (1000 * 60));
+  if (minutes < 60) {
+    return `    / ${minutes} minutes ago`;
+  }
+
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  return `     / ${hours} hours ago`;
+}
+
+function updateElapsedTime() {
+  const todos = document.querySelectorAll(".todo_item");
+  todos.forEach(todo => {
+    const timeSpan = todo.querySelector(".time_span");
+    const created = new Date(todo.getAttribute("data-created"));
+    timeSpan.innerText = getElapsedTime(created);
+  });
+}
+setInterval(updateElapsedTime, 60000);
 
 //Elimiar & Comprobar
 function deleteCheck(e) {
@@ -59,29 +106,15 @@ function deleteCheck(e) {
     todo.classList.toggle("completedItem");
   }
 }
-//FILTRAR LAS TAREAS CONFORME LAS OPCIONES
-function filterTodo(e) {
-  const todos = todoList.childNodes;
-  for (let i = 1; i < todos.length; i++) {
-    switch (e.target.value) {
-      case "all":
-        todos[i].style.display = "flex";
-        break;
-      case "completed":
-        if (todos[i].classList.contains("completedItem")) {
-          todos[i].style.display = "flex";
-        } else {
-          todos[i].style.display = "none";
-        }
-        break;
-      case "uncompleted":
-        if (!todos[i].classList.contains("completedItem")) {
-          todos[i].style.display = "flex";
-        } else {
-          todos[i].style.display = "none";
-        }
-        break;
-    }
-  }
+
+//Eliminar las tareas completadas
+const removeCompletedButton = document.querySelector(".remove_completed_btn");
+removeCompletedButton.addEventListener("click", removeCompleted);
+
+function removeCompleted() {
+  const completedItems = document.querySelectorAll(".completedItem");
+  completedItems.forEach(completedItem => {
+    completedItem.remove();
+  });
 }
 
